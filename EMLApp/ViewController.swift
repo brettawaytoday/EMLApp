6c5f67ff19c6ae10c1e0da1adadb870589cfa193
brettawaytoday <brettawaytoday@gmail.com>
2021-03-15 06:59:20 +1300

    Initial Commit

---
+++ EMLApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMLApp
+//
+//  Created by Brett Christian on 15/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
